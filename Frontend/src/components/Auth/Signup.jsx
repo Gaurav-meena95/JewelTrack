d246ecd101f2fd279a3e9435aee335d8b1c50bbc
Gaurav Meena <gaurav_meena@Gauravs-MacBook-Pro.local>
2025-12-29 14:52:41 +0530

    signup form frontend done

--- Frontend/src/components/Auth/Signup.jsx
+++ Frontend/src/components/Auth/Signup.jsx
@@ -1,26 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ArrowLeft, Gem } from 'lucide-react'
 import { motion } from 'motion/react'
 import ThemeToggle from '../ThemeToggle'
 
 const Signup = () => {
+    const[error,setError] = useState('')
+    const [loading,setLoading] = useState(false)
+    
+    const [formdata, setFormdata] = useState({
+        shopName: '',
+        name: '',
+        phone: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const handelChange = (e) => {
+        setFormdata({ ...formdata, [e.target.name]: e.target.value })
+    }
+    const handelSubmit = (e) => {
+        e.preventDefault()
+        setError('')
+    }
+
+
+
     return (
 
         <div className='min-h-screen bg-background flex items-center justify-center px-6 py-12 relative overflow-hidden'>
-            {/* <div
-                className="absolute inset-0 opacity-10"
-                style={{
-                    background: 'radial-gradient(circle at 50% 50%, var(--gold-glow) 0%, transparent 70%)',
-                }}
-            /> */}
 
             <div className="absolute top-6 right-6">
                 <ThemeToggle />
             </div>
 
             <button
-                onClick={() => onNavigate('shopkeeper-login')}
-                className="absolute top-6 left-6 flex items-center gap-2 text-muted-foreground hover:text-[var(--gold)] transition-colors"
+                className="absolute top-6 left-6 flex items-center gap-2 text-muted-foreground hover:text-[#c8b11c] transition-colors"
             >
                 <ArrowLeft className="h-5 w-5" />
                 Back to Login
@@ -40,10 +55,13 @@ const Signup = () => {
                     <h1 className='text-2xl mb-2'>Create Shop Account</h1>
                     <p className='text-muted-foreground'>Join our premium jewelry management platform</p>
                 </div>
-                <form className='mt-10'>
+                <form onSubmit={handelSubmit} className='mt-5'>
                     <div className='space-y2 my-3'>
                         <label htmlFor="ShopName">Shop Name</label>
                         <input type="text"
+                            name='shopName'
+                            onChange={handelChange}
+                            value={formdata.shopName}
                             id='ShopName'
                             placeholder="Luxury Jewels"
                             className='p-2 rounded-[8px] w-full bg-input/90 mt-1 backdrop-blur-sm border border-border/80 focus:border-[#513b01] focus:ring-amber-500 transition-all'
@@ -53,6 +71,9 @@ const Signup = () => {
                     <div className='space-y2 my-3'>
                         <label htmlFor="yourName">Your Name</label>
                         <input type="text"
+                            onChange={handelChange}
+                            name='name'
+                            value={formdata.name}
                             id='yourName'
                             placeholder="Gaurav Meena"
                             className='p-2 rounded-[8px] w-full bg-input/90 mt-1 backdrop-blur-sm border border-border/80 focus:border-[#513b01] focus:ring-amber-500 transition-all'
@@ -62,6 +83,9 @@ const Signup = () => {
                     <div className='space-y2 my-3'>
                         <label htmlFor="phoneNumber">Phone Number</label>
                         <input type="text"
+                            onChange={handelChange}
+                            name='phone'
+                            value={formdata.phone}
                             id='phoneNumber'
                             placeholder="+91 9849408389"
                             className='p-2 rounded-[8px] w-full bg-input/90 mt-1 backdrop-blur-sm border border-border/80 focus:border-[#513b01] focus:ring-amber-500 transition-all'
@@ -70,7 +94,10 @@ const Signup = () => {
                     </div>
                     <div className='space-y2 my-3'>
                         <label htmlFor="email">Email</label>
-                        <input type="text"
+                        <input type="email"
+                            onChange={handelChange}
+                            name='email'
+                            value={formdata.email}
                             id='email'
                             placeholder="jhon@email.com"
                             className='p-2 rounded-[8px] w-full bg-input/90 mt-1 backdrop-blur-sm border border-border/80 focus:border-[#513b01] focus:ring-amber-500 transition-all'
@@ -79,7 +106,10 @@ const Signup = () => {
                     </div>
                     <div className='space-y2 my-3'>
                         <label htmlFor="Password">Password</label>
-                        <input type="text"
+                        <input type="password"
+                            onChange={handelChange}
+                            name='password'
+                            value={formdata.password}
                             id='Password'
                             placeholder="•••••••••"
                             className='p-2 rounded-[5px] w-full bg-input/90 mt-1 backdrop-blur-sm border border-border/80 focus:border-[#513b01] focus:ring-amber-500 transition-all'
@@ -88,7 +118,10 @@ const Signup = () => {
                     </div>
                     <div className='space-y2 my-3'>
                         <label htmlFor="confirmPassword">Confirm Password</label>
-                        <input type="text"
+                        <input type="password"
+                            onChange={handelChange}
+                            name='confirmPassword'
+                            value={formdata.confirmPassword}
                             id='confirmPassword'
                             placeholder="•••••••••"
                             className='p-2 rounded-[8px] w-full bg-input/90 mt-1 backdrop-blur-sm border border-border/80 focus:border-[#513b01] focus:ring-amber-500 transition-all'
