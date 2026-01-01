4145a5195c95f3f51849e4ffbf2ed1beef56317e
Gaurav Meena <gaurav_meena@Gauravs-MacBook-Pro.local>
2026-01-01 19:11:22 +0530

    backend of Inventory is done with testing

--- Backend/module/Shopkeeper/Inventory/controllers.js
+++ Backend/module/Shopkeeper/Inventory/controllers.js
@@ -12,11 +12,12 @@ const createInventory = async (req, res) => {
             return res.status(403).json({ message: `Check missing value ${value}` })
         }
         const Shopkeeper= await User.find({_id:shopkeeper_id})
+        console.log('shopkeeper' ,Shopkeeper[0]._id)
         if (!Shopkeeper){
             return res.status(403).json({message:'shopkeeper does not exist'})
         }
 
-        const newInventory = await Inventory.create({shopkeeper:Shopkeeper.shopkeeper_id,jewelleryType, totalWeight, quantity ,metalType})
+        const newInventory = await Inventory.create({shopkeeper:Shopkeeper[0]._id,jewelleryType, totalWeight, quantity ,metalType})
         return res.status(200).json({message:'Inventory create successfully',newInventory})
 
     } catch (error) {
@@ -29,22 +30,19 @@ const createInventory = async (req, res) => {
 
 const updateInventory = async (req, res) => {
     try {
-        const {shopkeeper_id} = req.query
+        const {inventory_id} = req.query
         const { jewelleryType, totalWeight, quantity ,metalType } = req.body
         const value = validationInput({jewelleryType, totalWeight, quantity ,metalType})
         if (value) {
             return res.status(403).json({ message: `Check missing value ${value}` })
         }
 
-        const Shopkeeper= await User.find({_id:shopkeeper_id})
-        if (!Shopkeeper){
-            return res.status(403).json({message:'shopkeeper does not exist'})
-        }
+        const exsitingInventory = await Inventory.find({_id:inventory_id})
         const updated = await Inventory.updateOne(
-            { _id: exsitingCollateral[0]._id },
-            {shopkeeper:Shopkeeper.shopkeeper_id,jewelleryType, totalWeight, quantity ,metalType}
+            { _id: exsitingInventory[0]._id },
+            {jewelleryType, totalWeight, quantity ,metalType}
         )
-        return res.status(200).json({ message: "Collateral upadate successfully", updated })
+        return res.status(200).json({ message: "Inventory upadate successfully", updated })
     } catch (error) {
         console.log(error)
         return res.status(500).json({ message: 'Internal Server Error' })
@@ -53,17 +51,13 @@ const updateInventory = async (req, res) => {
 }
 const deleteInventory = async (req, res) => {
     try {
-        const { phone,Inventory_id } = req.query
-        const existing = Customer.findOne({ phone })
-        if (!existing) {
-            return res.status(402).json({ message: 'customer Inventory doest not exist' })
-        }
-        const exsitingCollateral = await Collateral.find({_id:Inventory_id})
-        if (exsitingCollateral.length === 0){
+        const { inventory_id } = req.query
+        const exsitingInventory = await Inventory.find({_id:inventory_id})
+        if (exsitingInventory.length === 0){
             return res.status(401).json({message:'Inventory does not exist'})
         }
 
-       const deleted =  await Collateral.deleteOne({ _id: exsitingCollateral[0]._id })
+       const deleted =  await Inventory.deleteOne({ _id: exsitingInventory[0]._id })
         return res.status(200).json({ message: 'Inventory successfully deleted' ,deleted})
     } catch (error) {
         console.log(error)
@@ -78,7 +72,7 @@ const allInventory = async (req, res) => {
         if (!Shopkeeper){
             return res.status(403).json({message:'shopkeeper does not exist'})
         }
-        const allInventorys = await Inventory.find({shopkeeper:Shopkeeper.id})
+        const allInventorys = await Inventory.find({shopkeeper:Shopkeeper[0]._id})
         return res.status(200).json({ message: "All Inventory are :", allInventorys })
 
 
